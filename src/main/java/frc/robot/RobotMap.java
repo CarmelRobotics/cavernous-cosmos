cf6d5b4fcdb7b5a462cecc68693805ee4fbc45e6
Robert Paul <rob.k.paul@gmail.com>
2019-01-26 10:20:27 -0800

    Coded the Wheeldrop

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,13 +14,9 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+  
+  // --Pneumatic Control IDs--
+  public static final int DROP_PCM_ID_FW = 0;
+  public static final int DROP_PCM_ID_BW = 0;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
 }
---
+++ src/main/java/frc/robot/subsystems/WheelDrop.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+/**
+ * Used to drop the center wheel for slide drive.
+ */
+public class WheelDrop extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  // Somebody once told me
+  
+  private DoubleSolenoid dropper;
+
+  public void initialize(){
+    dropper = new DoubleSolenoid(RobotMap.DROP_PCM_ID_FW, RobotMap.DROP_PCM_ID_BW);
+  }
+
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+  
+  public void down(){ //frick
+    dropper.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void up(){ // up and away
+    dropper.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void stop(){ // hammer time
+    dropper.set(DoubleSolenoid.Value.kOff);
+  }
+}
