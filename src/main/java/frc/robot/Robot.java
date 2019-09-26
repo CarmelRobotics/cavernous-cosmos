0c98c44d0396024ee9ab329ac2dad689c548b8b3
Robert Paul <rob.k.paul@gmail.com>
2019-09-26 10:42:35 -0700

    Added Print Statement to get val of Ground Switch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -132,6 +132,7 @@ public class Robot extends TimedRobot {
     if(USE_PI_VISION)
       vision.VisionPeriodic();
     //System.out.println(RobotMap.PISTON_OUT);
+    System.out.println(arm.getGroundSwitch());
   }
 
   @Override
