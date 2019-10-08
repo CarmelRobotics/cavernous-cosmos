c2a039ba699ae9a755b32ed9609b19585e445d34
Robert Paul <rob.k.paul@gmail.com>
2019-10-08 11:14:35 -0700

    Removed Print Statements

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -132,8 +132,6 @@ public class Robot extends TimedRobot {
     if(USE_PI_VISION)
       vision.VisionPeriodic();
     //System.out.println(RobotMap.PISTON_OUT);
-    System.out.println("TOP" + arm.getTopLimit());
-    System.out.println("ANGLE" + arm.getAngleLimit());
   }
 
   @Override
