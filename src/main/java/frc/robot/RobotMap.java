2208c3e72f047a47db11b5c83d048a7a20a7ae9d
Robert Paul <rob.k.paul@gmail.com>
2019-10-11 11:01:04 -0700

    added values for Limit Switches that did not have values

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -97,8 +97,8 @@ public class RobotMap {
 
   /* Limit Switches */
     public static final int ELEV_BOTTOM_LIMIT = 0;
-    public static final int ARM_ANGLE_LIMIT = 1;
-    public static final int INTAKE_STOP_LIMIT = 2; 
+    public static final int ARM_ANGLE_LIMIT = 2;
+    public static final int INTAKE_STOP_LIMIT = 1; 
     
   /* Pneumatic Control IDs */
     public static final int WHEELDROP_PCM_ID_UP = 2;
